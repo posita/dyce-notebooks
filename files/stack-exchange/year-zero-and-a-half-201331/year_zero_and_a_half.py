12c41036838f99625d3f76c741c3b0424a742b69
posita <eb3f73+github+com@yaymail.com>
2022-09-15 01:17:22 +0000

    Deploying to gh-pages from @ posita/dyce-notebooks@95723a9dec9aeddb8dc03d20fd5bcffdcdf606d6 🚀

--- files/stack-exchange/year-zero-and-a-half-201331/year_zero_and_a_half.py
+++ files/stack-exchange/year-zero-and-a-half-201331/year_zero_and_a_half.py
@@ -13,56 +13,79 @@ class YearZeroOutcomes(IntEnum):
     FULL_SUCCESS = auto()
 
 
-dyz = P(
+# Our legacy die. This is equivalent to a fair, six-sided die with one face showing a
+# bane, one face showing a success, and four blank faces.
+dyz_legacy = P(
     H(
-        {
-            YearZeroOutcomes.BANE: 1,
-            YearZeroOutcomes.BLANK: 3,
-            YearZeroOutcomes.HALF_SUCCESS: 1,
-            YearZeroOutcomes.FULL_SUCCESS: 1,
-        }
+        (
+            YearZeroOutcomes.BANE,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.FULL_SUCCESS,
+        )
     )
 )
 
-dyz_legacy = P(
+# Our modified die. This is equivalent to a fair, six-sided die with one face showing a
+# bane, one face showing a success, one face showing a "half" success, and three blank
+# faces.
+dyz = P(
     H(
-        {
-            YearZeroOutcomes.BANE: 1,
-            YearZeroOutcomes.BLANK: 4,
-            YearZeroOutcomes.FULL_SUCCESS: 1,
-        }
+        (
+            YearZeroOutcomes.BANE,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.BLANK,
+            YearZeroOutcomes.HALF_SUCCESS,
+            YearZeroOutcomes.FULL_SUCCESS,
+        )
     )
 )
 
 
 @expandable
 def count_successes(pool: PResult) -> Union[H, float]:
+    r"""
+    When provided a pool of one or more of one of our above dice, this computes the
+    distribution with the expected number of successes as outcomes. When using our
+    modified die, this can include "half" successes (each of which contribute 0.5 to the
+    number of successes).
+    """
     counter = Counter(pool.roll)
-    successes = (
-        counter[YearZeroOutcomes.HALF_SUCCESS]
-        + 2 * counter[YearZeroOutcomes.FULL_SUCCESS]
-    ) / 2
+    successes_for_this_roll = (
+        counter[YearZeroOutcomes.HALF_SUCCESS] / 2
+        + counter[YearZeroOutcomes.FULL_SUCCESS]
+    )
 
-    return successes
+    return successes_for_this_roll
 
 
 def count_successes_with_push(pool: P) -> H:
+    r"""
+    When provided a pool of one or more of one of our above dice, this computes the
+    distribution after a push with the expected number of successes as outcomes. Like
+    count_successes above, when using our modified die, this can include "half"
+    successes (each of which contribute 0.5 to the number of successes).
+    """
+
     @expandable
     def _expand(p_result: PResult) -> Union[H, float]:
         counter = Counter(p_result.roll)
-        blanks = counter[YearZeroOutcomes.BLANK]
-        successes_so_far = (
-            counter[YearZeroOutcomes.HALF_SUCCESS]
-            + 2 * counter[YearZeroOutcomes.FULL_SUCCESS]
-        ) / 2
+        blanks_this_roll = counter[YearZeroOutcomes.BLANK]
+        successes_for_this_roll = (
+            counter[YearZeroOutcomes.HALF_SUCCESS] / 2
+            + counter[YearZeroOutcomes.FULL_SUCCESS]
+        )
         assert p_result.p.is_homogeneous()
 
-        if blanks:
-            reroll = count_successes(blanks @ p_result.p[:1])
+        if blanks_this_roll:
+            push = count_successes(blanks_this_roll @ p_result.p[:1])
 
-            return reroll + successes_so_far
+            return push + successes_for_this_roll
         else:
-            return successes_so_far
+            return successes_for_this_roll
 
     return _expand(
         pool,
@@ -72,28 +95,37 @@ def count_successes_with_push(pool: P) -> H:
 
 @expandable
 def count_banes(pool: PResult) -> Union[H, float]:
+    r"""
+    When provided a pool of one or more of one of our above dice, this computes the
+    distribution with the expected number of banes as outcomes.
+    """
     counter = Counter(pool.roll)
-    banes = counter[YearZeroOutcomes.BANE]
+    banes_this_roll = counter[YearZeroOutcomes.BANE]
 
-    return banes
+    return banes_this_roll
 
 
 def count_banes_with_push(pool: P) -> H:
+    r"""
+    When provided a pool of one or more of one of our above dice, this computes the
+    distribution after a push with the expected number of banes as outcomes.
+    """
+
     @expandable
     def _expand(p_result: PResult) -> Union[H, int]:
         counter = Counter(p_result.roll)
-        blanks = counter[YearZeroOutcomes.BLANK]
-        banes_so_far = counter[YearZeroOutcomes.BANE]
+        blanks_this_roll = counter[YearZeroOutcomes.BLANK]
+        banes_this_roll = counter[YearZeroOutcomes.BANE]
         assert p_result.p.is_homogeneous()
 
-        if blanks:
-            reroll = count_banes(pool=blanks @ p_result.p[:1])
+        if blanks_this_roll:
+            push = count_banes(pool=blanks_this_roll @ p_result.p[:1])
 
-            return reroll + banes_so_far
+            return push + banes_this_roll
         else:
-            return banes_so_far
+            return banes_this_roll
 
     return _expand(
         pool,
-        limit=2,  # to ensure count_successes is called in the interior
+        limit=2,  # to ensure count_banes is called in the interior
     )
