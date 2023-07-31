c67f1f7e6cbc5832e0882767384db88edd175a15
posita <eb3f73+github+com@yaymail.com>
2023-07-31 01:47:56 +0000

    Deploying to gh-pages from @ posita/dyce-notebooks@6c849cdc80a27627233fab4b9a59acd46018906f 🚀

--- files/github/bumpity-pool-posita-dyce-12/dyce_impl.py
+++ files/github/bumpity-pool-posita-dyce-12/dyce_impl.py
@@ -1,8 +1,8 @@
-from enum import IntEnum, auto
+import unittest
 from functools import cache
 
 from dyce import H, P
-from dyce.evaluation import HResult, PResult, explode, foreach
+from dyce.evaluation import HResult, PResult, PWithSelection, explode, foreach
 
 try:
     from dyce.evaluation import LimitT
@@ -15,78 +15,83 @@ from params import Params
 __all__ = ()
 
 
-class Pool(IntEnum):
-    STANDARD = auto()
-    BUMP = auto()
+def mechanic_dyce_fudged(params: Params, die: H, explode_limit: LimitT = 0) -> H:
+    return mechanic_dyce_base(params, die) + _aggregate_exploded_deltas(
+        die, explode_limit
+    )
 
 
-assert Pool.STANDARD < Pool.BUMP
-
-
-def mechanic_dyce_fudged(
-    params: Params,
-    die: H,
-    explode_limit: LimitT = 0,
-) -> H:
+def mechanic_dyce_base(params: Params, die: H) -> H:
     r"""
-    *set_die* is the zero-based index into each roll (i.e., on the interval ``#!python
-    [0, num_std + num_bmp)``) for determining the check die. *num_std* is how many of
-    *die* are in the standard pool. *num_bmp* is how many of *die* are in the bump pool.
-    Note this interface only allows for homogeneous pools. *bonus_dice* are an optional
-    sequence of zero-based indexes into each roll whose values are added to the total.
-    *explode_limit* has same meaning as ``#!python dyce.evaluation.expandable``.
+    *params* and *die* are used to describe the pool and mechanic constraints. Note this
+    interface only allows for homogeneous pools. *explode_limit* has same meaning as
+    ``#!python dyce.evaluation.expandable``.
     """
     pool_size = params.num_std + params.num_bmp
     extra_std = min(params.extra_std, pool_size)
     extra_bmp = min(params.extra_bmp, pool_size)
-
-    if params.extra_std:
-        extra_bonus = -2 * max(params.extra_std - pool_size, 0)
-        roll_slice = slice(None, -extra_std)
-    elif params.extra_bmp:
-        extra_bonus = 2 * max(params.extra_bmp - pool_size, 0)
-        roll_slice = slice(extra_bmp, None)
-    else:
-        extra_bonus = 0
-        roll_slice = slice(None)
-
     p_std = (params.num_std + extra_std) @ P(die)
     p_bmp = (params.num_bmp + extra_bmp) @ P(die)
 
+    if params.extra_std:
+        extra_bonus = -2 * max(params.extra_std - pool_size, 0)
+        roll_slice = slice(None, pool_size)
+    elif params.extra_bmp:
+        extra_bonus = 2 * max(params.extra_bmp - pool_size, 0)
+        roll_slice = slice(-pool_size, None)
+    else:
+        extra_bonus = 0
+        roll_slice = slice(None, pool_size)
+
     def _mechanic(std: PResult, bmp: PResult | None = None):
-        roll = list((outcome, Pool.STANDARD) for outcome in std.roll)
+        # double each standard outcome (all even; assumes all outcomes are integers and support
+        # bit-wise operations)
+        roll = [outcome << 1 for outcome in std.roll]  # faster than outcome * 2
 
         if bmp is not None:
-            roll.extend((outcome, Pool.BUMP) for outcome in bmp.roll)
+            # double each bump outcome and add one (all odd)
+            roll.extend(
+                outcome << 1 | 1 for outcome in bmp.roll  # faster than outcome * 2 + 1
+            )
+            roll.sort()
+            roll = roll[roll_slice]
 
-        roll.sort()
-        roll = roll[roll_slice]
         assert len(roll) == pool_size
         check_die = params.set_die
-        check_outcome, set_type = roll[check_die]
+        shifted_check_outcome = roll[check_die]
 
-        if set_type is Pool.BUMP:
-            check_die = (check_die + 1) % len(roll)
-            check_outcome, _ = roll[check_die]
+        # check to see if the shifted check outcome is odd; if so, it's a bump die
+        if shifted_check_outcome & 0x1:  # faster than check_outcome % 2 == 1
+            check_die = (check_die + 1) % pool_size
+            shifted_check_outcome = roll[check_die]
 
-        total_outcome = check_outcome
+        # add the original value for the check outcome to the total
+        total_outcome = shifted_check_outcome >> 1  # faster than outcome // 2
 
         if check_die < params.set_die:
-            wrapped_outcome, _ = roll[params.set_die]
+            wrapped_outcome = roll[params.set_die] >> 1  # faster than outcome // 2
             total_outcome += wrapped_outcome
 
         return (
             total_outcome
-            + sum(roll[bonus_die][0] for bonus_die in params.bonus_dice)
+            # halve each bonus outcome (restores original value)
+            + sum(
+                roll[bonus_die] >> 1  # faster than outcome // 2
+                for bonus_die in params.bonus_dice
+            )
             + extra_bonus
         )
 
     if p_bmp:
-        unexploded_result = foreach(_mechanic, p_std, p_bmp)
+        unexploded_result = foreach(
+            _mechanic,
+            PWithSelection(p_std, (roll_slice,)),
+            PWithSelection(p_bmp, (roll_slice,)),
+        )
     else:
-        unexploded_result = foreach(_mechanic, p_std)
+        unexploded_result = foreach(_mechanic, PWithSelection(p_std, (roll_slice,)))
 
-    return unexploded_result + _aggregate_exploded_deltas(die, explode_limit)
+    return unexploded_result
 
 
 @cache
@@ -111,3 +116,185 @@ def _explosions_by_outcome(die: H, explode_limit: LimitT) -> dict[int, H]:
             )
             for outcome in die
         }
+
+
+class TestExplosions(unittest.TestCase):
+    def test_explosions_by_outcome(self):
+        d2 = H(2)
+        self.assertEqual(_explosions_by_outcome(d2, explode_limit=0), {})
+        self.assertEqual(
+            _explosions_by_outcome(d2, explode_limit=1),
+            {
+                # H(2).eq(1) -> H({False: 1, True: 1}) ; explode(H(2), limit=1) -> H({2: 3, 3: 1})  # i.e., 1+1 (2) * 1, 1+2 (3) * 1, 2 * 2
+                1: H({0: 4, 2: 3, 3: 1}),
+                # H(2).eq(2) -> H({False: 1, True: 1}) ; explode(H(2), limit=1) -> H({2: 3, 3: 1})  # i.e., 1 * 2, 2+1 (3) * 1, 2+2 (4) * 1
+                2: H({0: 4, 1: 2, 3: 1, 4: 1}),
+            },
+        )
+        self.assertEqual(
+            _explosions_by_outcome(H({1: 1, 2: 2}), explode_limit=1),
+            {
+                # H({1: 1, 2: 2}).eq(1) -> H({False: 2, True: 1}) ; explode(H({1: 1, 2: 2}), limit=1) -> H({2: 7, 3: 2})  # i.e., 1+1 (2) * 1, 1+2 (3) * 2, 2 * 6
+                1: H({0: 18, 2: 7, 3: 2}),
+                # H({1: 1, 2: 2}).eq(2) -> H({False: 1, True: 2}) ; explode(H({1: 1, 2: 2}), limit=1) -> H({1: 3, 2: 2, 3: 4})  # i.e., 1 * 3, 2+1 (3) * 2, 2+2 (4) * 4
+                2: H({0: 9, 1: 6, 3: 4, 4: 8}),
+            },
+        )
+        self.assertEqual(
+            _explosions_by_outcome(d2, explode_limit=2),
+            {
+                # H(2).eq(1) -> H({False: 1, True: 1}) ; explode(H(2), limit=1) -> H({2: 4, 3: 3, 4: 1})  # i.e., 1+1+1 (3) * 1, 1+1+2 (4) * 1, 1+2 (3) * 2, 2 * 4
+                1: H({0: 8, 2: 4, 3: 3, 4: 1}),
+                # H(2).eq(2) -> H({False: 1, True: 1}) ; explode(H(2), limit=1) -> H({1: 4, 3: 2, 5: 1, 6: 1})  # i.e., 1 * 4, 2+1 (3) * 2, 2+2+1 (5) * 1, 2+2+2 (6) * 1
+                2: H({0: 8, 1: 4, 3: 2, 5: 1, 6: 1}),
+            },
+        )
+
+    def test_aggregate_exploded_deltas(self):
+        d2 = H(2)
+        self.assertEqual(_aggregate_exploded_deltas(d2, 0), H({0: 1}))
+        self.assertEqual(_aggregate_exploded_deltas(d2, 1), H({0: 2, 1: 1, 2: 1}))
+        self.assertEqual(
+            _aggregate_exploded_deltas(H({1: 1, 2: 2}), 1), H({0: 12, 1: 5, 2: 10})
+        )
+        self.assertEqual(
+            _aggregate_exploded_deltas(d2, 2), H({0: 8, 1: 2, 2: 3, 3: 2, 4: 1})
+        )
+
+
+class TestMechanic(unittest.TestCase):
+    def test_base_simple(self):
+        d2 = H(2)
+
+        notation = "1s0b@1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), d2)
+
+        notation = "1s1b@2"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), H({2: 2, 3: 1, 4: 1}))
+
+        notation = "1s0b@1>1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), H({1: 1, 2: 3}))
+
+        notation = "1s0b@1<1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), H({1: 3, 2: 1}))
+
+        notation = "1s0b@1+@1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), 2 * d2)
+
+        notation = "1s0b@1>1+@1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), H({2: 1, 4: 3}))
+
+        notation = "1s0b@1<1+@1"
+        (params,) = Params.parse_from_notation(notation)
+        self.assertEqual(mechanic_dyce_base(params, d2), H({2: 3, 4: 1}))
+
+    def test_base_options(self):
+        d6 = H(6)
+
+        for notation, expected in (
+            ("4s1b@4", H({1: 3, 2: 101, 3: 409, 4: 879, 5: 1283, 6: 1213})),
+            ("4s1b@3", H({1: 21, 2: 171, 3: 330, 4: 382, 5: 291, 6: 101})),
+            ("5s0b@1", H({1: 4651, 2: 2101, 3: 781, 4: 211, 5: 31, 6: 1})),
+            ("5s0b@3", H({1: 23, 2: 113, 3: 188, 4: 188, 5: 113, 6: 23})),
+            ("5s0b@5", H({1: 1, 2: 31, 3: 211, 4: 781, 5: 2101, 6: 4651})),
+            ("4s1b@1", H({1: 671, 2: 369, 3: 175, 4: 65, 5: 15, 6: 1})),
+            ("4s1b@2", H({1: 513, 2: 1199, 3: 1123, 4: 717, 5: 293, 6: 43})),
+            ("4s1b@3", H({1: 21, 2: 171, 3: 330, 4: 382, 5: 291, 6: 101})),
+            ("4s1b@4", H({1: 3, 2: 101, 3: 409, 4: 879, 5: 1283, 6: 1213})),
+            (
+                "4s1b@5",
+                H(
+                    {
+                        2: 16,
+                        3: 145,
+                        4: 591,
+                        5: 1666,
+                        6: 3790,
+                        7: 861,
+                        8: 435,
+                        9: 190,
+                        10: 66,
+                        11: 15,
+                        12: 1,
+                    }
+                ),
+            ),
+            ("1s0b@1>2", H({3: 1, 4: 3, 5: 5, 6: 7, 7: 9, 8: 11})),
+            ("1s0b@1<2", H({-1: 11, 0: 9, 1: 7, 2: 5, 3: 3, 4: 1})),
+            ("1s2b@2", H({1: 1, 2: 15, 3: 29, 4: 43, 5: 57, 6: 71})),
+            ("1s2b@1>1", H({1: 7, 2: 57, 3: 98, 4: 118, 5: 105, 6: 47})),
+            ("1s2b@1<1", H({1: 421, 2: 363, 3: 269, 4: 163, 5: 69, 6: 11})),
+            (
+                "1s2b@1+@3",
+                H(
+                    {
+                        2: 1,
+                        3: 4,
+                        4: 9,
+                        5: 16,
+                        6: 25,
+                        7: 36,
+                        8: 35,
+                        9: 32,
+                        10: 27,
+                        11: 20,
+                        12: 11,
+                    }
+                ),
+            ),
+            (
+                "1s2b@1>1+@3",
+                H(
+                    {
+                        2: 1,
+                        3: 4,
+                        4: 15,
+                        5: 35,
+                        6: 71,
+                        7: 123,
+                        8: 195,
+                        9: 239,
+                        10: 252,
+                        11: 220,
+                        12: 141,
+                    }
+                ),
+            ),
+            (
+                "1s2b@1<1+@3",
+                H(
+                    {
+                        2: 21,
+                        3: 80,
+                        4: 147,
+                        5: 208,
+                        6: 237,
+                        7: 216,
+                        8: 163,
+                        9: 112,
+                        10: 69,
+                        11: 32,
+                        12: 11,
+                    }
+                ),
+            ),
+        ):
+            try:
+                (params,) = Params.parse_from_notation(notation)
+            except Exception as exc:
+                raise AssertionError(
+                    f"unable to parse notation ({notation!r})"
+                ) from exc
+
+            actual = mechanic_dyce_base(params, d6)
+            self.assertEqual(expected, actual, msg=f"notation = {notation!r}")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- files/github/bumpity-pool-posita-dyce-12/params.py
+++ files/github/bumpity-pool-posita-dyce-12/params.py
@@ -1,5 +1,6 @@
 import re
 import traceback
+import unittest
 from dataclasses import dataclass
 from typing import ClassVar, Iterator
 
@@ -13,9 +14,9 @@ class Params:
     num_std: int
     num_bmp: int
     set_die: int  # zero-indexed, i.e., [0, num_std + num_bmp)
-    bonus_dice: tuple[int, ...] = ()
     extra_std: int = 0
     extra_bmp: int = 0
+    bonus_dice: tuple[int, ...] = ()
     comment: str = ""
     override_die: H | None = None
     override_die_str: str | None = None
@@ -81,9 +82,9 @@ class Params:
                     num_std,
                     num_bmp,
                     set_die,
-                    bonus_dice,
                     ex_std,
                     ex_bmp,
+                    bonus_dice,
                     comment,
                     override_die_map[override_str] if override_str else None,  # type: ignore
                     override_str or None,
@@ -105,27 +106,345 @@ class Params:
 
     def __post_init__(self) -> None:
         pool_size = self.num_std + self.num_bmp
-        assert (
-            0 <= self.set_die < pool_size
-        ), "the set die must refer to a die within the pool ({self.set_die})"
-        assert (
-            self.num_std >= 1
-        ), f"the standard pool must contain at least one die ({self.num_std})"
-        assert (
-            self.num_bmp >= 0
-        ), f"the bump pool must contain at least zero dice ({self.num_bmp})"
+
+        if self.num_std < 1:
+            raise ValueError(
+                f"the standard pool must contain at least one die ({self.num_std})"
+            )
+
+        if self.num_bmp < 0:
+            raise ValueError(
+                f"the bump pool must contain at least zero dice ({self.num_bmp})"
+            )
+
+        if not (0 <= self.set_die < pool_size):
+            raise ValueError(
+                f"the set die must refer to a die within the pool ({self.set_die})"
+            )
 
         # Normalize relative differences in extra_std and extra_bmp so that one is zero
         # and the other is greater-than-or-equal-to zero
         extra_lowest = min(self.extra_std, self.extra_bmp)
         self.extra_std -= extra_lowest
         self.extra_bmp -= extra_lowest
-
         assert (
             self.extra_std >= 0
             and self.extra_bmp >= 0
             and 0 in (self.extra_std, self.extra_bmp)
         ), f"normalization failed ({self.extra_std}, {self.extra_bmp})"
-        assert all(
-            0 <= bonus_die < pool_size for bonus_die in self.bonus_dice
-        ), f"each bonus die must refer to a die within the pool ({self.bonus_dice})"
+
+        self.bonus_dice = tuple(self.bonus_dice)
+
+        if not all(0 <= bonus_die < pool_size for bonus_die in self.bonus_dice):
+            raise ValueError(
+                f"each bonus die must refer to a die within the pool {self.bonus_dice}"
+            )
+
+
+class TestParams(unittest.TestCase):
+    def test_bad_bonus_die(self):
+        good_bonus_dice = tuple(range(3))
+        extras = tuple(range(3))
+
+        for bad_bonus_die in (-1, 3):
+            bonus_dice = good_bonus_dice + (bad_bonus_die,)
+
+            with self.assertRaisesRegex(
+                ValueError,
+                rf"\Aeach bonus die must refer to a die within the pool {re.escape(str(bonus_dice))}\Z",
+                msg=f"bonus_dice = {bonus_dice}; bad_bonus_die = {bad_bonus_die}",
+            ):
+                Params(1, 2, 0, bonus_dice=bonus_dice)
+
+            for extra_std in extras:
+                with self.assertRaisesRegex(
+                    ValueError,
+                    rf"\Aeach bonus die must refer to a die within the pool {re.escape(str(bonus_dice))}\Z",
+                    msg=f"extra_std = {extra_std}; bonus_dice = {bonus_dice}; bad_bonus_die = {bad_bonus_die}",
+                ):
+                    Params(1, 2, 0, extra_std=extra_std, bonus_dice=bonus_dice)
+
+            for extra_bmp in extras:
+                with self.assertRaisesRegex(
+                    ValueError,
+                    rf"\Aeach bonus die must refer to a die within the pool {re.escape(str(bonus_dice))}\Z",
+                    msg=f"extra_bmp = {extra_bmp}; bonus_dice = {bonus_dice}; bad_bonus_die = {bad_bonus_die}",
+                ):
+                    Params(1, 2, 0, extra_bmp=extra_bmp, bonus_dice=bonus_dice)
+
+    def test_bad_notation(self):
+        override_die_map = {
+            "d10": H(10),
+            "d20": H(20),
+        }
+
+        for notation in (
+            " nonsense 1s 0b @1",  # bad line
+            " 1s 0b @1 nonsense ",  # bad line
+            "0s 0b @1",  # bad num_std
+            "1s 0b @0",  # bad set_die
+            "1s 0b @1 <0",  # bad extra_std
+            "1s 0b @1 >0",  # bad extra_bmp
+            "1s 0b @1 +@0",  # bad bonus
+            "[d8] 1s 0b @1 +@0",  # missing override
+        ):
+            self.assertEqual(
+                len(tuple(Params.parse_from_notation(notation, override_die_map))),
+                0,
+                msg=f"notation = {notation!r}",
+            )
+
+    def test_bad_num_bmp(self):
+        with self.assertRaisesRegex(
+            ValueError,
+            r"\Athe bump pool must contain at least zero dice \(-1\)\Z",
+        ):
+            Params(2, -1, 0)
+
+    def test_bad_num_std(self):
+        for num_std in (-1, 0):
+            with self.assertRaisesRegex(
+                ValueError,
+                rf"\Athe standard pool must contain at least one die \({re.escape(str(num_std))}\)\Z",
+                msg=f"num_std = {num_std}",
+            ):
+                Params(num_std, 1, 0)
+
+    def test_bad_set_die(self):
+        for set_die in (-1, 1):
+            with self.assertRaisesRegex(
+                ValueError,
+                rf"\Athe set die must refer to a die within the pool \({re.escape(str(set_die))}\)\Z",
+                msg=f"set_die = {set_die}",
+            ):
+                Params(1, 0, set_die)
+
+    def test_bonus_iter(self):
+        params = Params(3, 0, 0, bonus_dice=range(3))
+        self.assertEqual(params.bonus_dice, (0, 1, 2))
+
+    def test_defaults(self):
+        params = Params(1, 0, 0)
+        self.assertEqual(params.bonus_dice, ())
+        self.assertEqual(params.extra_std, 0)
+        self.assertEqual(params.extra_bmp, 0)
+        self.assertEqual(params.comment, "")
+        self.assertIsNone(params.override_die)
+        self.assertIsNone(params.override_die_str)
+
+    def test_extra(self):
+        params = Params(1, 0, 0, extra_std=-1, extra_bmp=-1)
+        self.assertEqual(params.extra_std, 0)
+        self.assertEqual(params.extra_bmp, 0)
+
+        params = Params(1, 0, 0, extra_std=-2, extra_bmp=-1)
+        self.assertEqual(params.extra_std, 0)
+        self.assertEqual(params.extra_bmp, 1)
+
+        params = Params(1, 0, 0, extra_std=2, extra_bmp=-1)
+        self.assertEqual(params.extra_std, 3)
+        self.assertEqual(params.extra_bmp, 0)
+
+    def test_minimal(self):
+        params = Params(1, 0, 0)
+        self.assertEqual(params.num_std, 1)
+        self.assertEqual(params.num_bmp, 0)
+        self.assertEqual(params.set_die, 0)
+
+    def test_notation(self):
+        default_extra_std = 0
+        default_extra_bmp = 0
+        default_bonus_dice = ()
+        default_comment = ""
+        default_override_die = None
+        default_override_die_str = None
+
+        override_die_map = {
+            "d10": H(10),
+            "d20": H(20),
+        }
+
+        for (
+            notation,
+            notation_str,
+            num_std,
+            num_bmp,
+            set_die,
+            extra_std,
+            extra_bmp,
+            bonus_dice,
+            comment,
+            override_die,
+            override_die_str,
+        ) in (
+            (
+                "1s 0b @1",
+                "1s0b@1",
+                1,  # num_std
+                0,  # num_bmp
+                0,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                default_bonus_dice,
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 2b @2",
+                "1s2b@2",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                default_bonus_dice,
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 2b @2 >2",
+                "1s2b@2>2",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                default_extra_std,
+                2,  # extra_bmp
+                default_bonus_dice,
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 2b @2 <2",
+                "1s2b@2<2",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                2,  # extra_std
+                default_extra_bmp,
+                default_bonus_dice,
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 2b @2 +@1",
+                "1s2b@2+@1",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                (0,),  # bonus_dice
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 2b @2 +@1 +@2 +@1",
+                "1s2b@2+@1+@2+@1",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                (0, 1, 0),  # bonus_dice
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 0b @1#  ",
+                "1s0b@1",
+                1,  # num_std
+                0,  # num_bmp
+                0,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                default_bonus_dice,
+                default_comment,
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "1s 0b @1  # hellooo  ",
+                "1s0b@1  # hellooo",
+                1,  # num_std
+                0,  # num_bmp
+                0,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                default_bonus_dice,
+                "hellooo",  # comment
+                default_override_die,
+                default_override_die_str,
+            ),
+            (
+                "[d20] 1s 0b @1",
+                "[d20]1s0b@1",
+                1,  # num_std
+                0,  # num_bmp
+                0,  # set_die
+                default_extra_std,
+                default_extra_bmp,
+                default_bonus_dice,
+                default_comment,
+                override_die_map["d20"],
+                "d20",  # override_die_str
+            ),
+            (
+                "[d10] 1s 2b @2 >2 +@1 +@2 +@1  # hellooo  ",
+                "[d10]1s2b@2>2+@1+@2+@1  # hellooo",
+                1,  # num_std
+                2,  # num_bmp
+                1,  # set_die
+                0,  # extra_std
+                2,  # extra_bmp
+                (0, 1, 0),  # bonus_dice
+                "hellooo",  # comment
+                override_die_map["d10"],
+                "d10",  # override_die_str
+            ),
+        ):
+            try:
+                (params,) = Params.parse_from_notation(notation, override_die_map)
+            except Exception as exc:
+                raise AssertionError(
+                    f"unable to parse notation ({notation!r})"
+                ) from exc
+
+            self.assertEqual(str(params), notation_str, msg=f"notation = {notation!r}")
+            self.assertEqual(params.num_std, num_std, msg=f"notation = {notation!r}")
+            self.assertEqual(params.num_bmp, num_bmp, msg=f"notation = {notation!r}")
+            self.assertEqual(params.set_die, set_die, msg=f"notation = {notation!r}")
+            self.assertEqual(
+                params.extra_std, extra_std, msg=f"notation = {notation!r}"
+            )
+            self.assertEqual(
+                params.extra_bmp, extra_bmp, msg=f"notation = {notation!r}"
+            )
+            self.assertEqual(
+                params.bonus_dice, bonus_dice, msg=f"notation = {notation!r}"
+            )
+            self.assertEqual(params.comment, comment, msg=f"notation = {notation!r}")
+
+            if override_die is None:
+                self.assertIsNone(params.override_die, msg=f"notation = {notation!r}")
+                self.assertIsNone(
+                    params.override_die_str, msg=f"notation = {notation!r}"
+                )
+            else:
+                self.assertEqual(
+                    params.override_die, override_die, msg=f"notation = {notation!r}"
+                )
+                self.assertEqual(
+                    params.override_die_str,
+                    override_die_str,
+                    msg=f"notation = {notation!r}",
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
